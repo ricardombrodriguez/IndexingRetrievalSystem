cb20a37b2adeea7df13eb40196400d621b925f71
Ricardo Rodriguez <ricardorodriguez@ua.pt>
2022-10-09 16:59:14 +0100

    reader

--- reader.py
+++ reader.py
@@ -1,5 +1,6 @@
 import gzip
 import json
+import sys
 from utils import dynamically_init_class
 
 
@@ -22,7 +23,21 @@ class PubMedReader(Reader):
         super().__init__(**kwargs)
         print("init PubMedReader|", f"{self.path_to_collection=}", )
 
+        terms = {}
+
         with gzip.open(self.path_to_collection, mode="rt") as f:
-            data = f.read()
-        print(len(data))
-        print(data[1:3000])
+
+            for pub in f:
+
+                pub_json = json.loads(pub)
+                pmid = pub_json['pmid']
+                pub_terms = set(pub_json['title'].split() + pub_json['abstract'].split())
+                for term in pub_terms:
+                    if term not in terms:
+                        terms[term] = [pmid]
+                    else:
+                        terms[term] += [pmid]
+
+        print("The size of the dictionary is {} MBs".format(sys.getsizeof(terms)/1000000))
+        print(terms["science"])
+    
