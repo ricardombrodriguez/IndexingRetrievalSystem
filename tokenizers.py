dca22dbf00bf452fd0657aae760a1f421ac67e98
goncalo-leal <goncalolealsilva@ua.pt>
2022-10-16 21:17:59 +0100

    refining regex

--- tokenizers.py
+++ tokenizers.py
@@ -1,7 +1,6 @@
 from utils import dynamically_init_class
 import nltk
 import re
-import regex
 from os.path import exists
 
 def dynamically_init_tokenizer(**kwargs):
@@ -20,8 +19,7 @@ class Tokenizer:
         filtered_terms = {}
         for term in terms:                                                      # ABSTRACT.()awesome
             lower_term = term.lower()                                           # abstract.()awesome                                                      
-            filtered_term = re.sub('[^a-zA-Z\d\s:]',' ',lower_term)
-            filtered_term = regex.sub('(?<=[a-zA-Z]+)-(?=[a-zA-Z]+)', ' ', filtered_term)  # abstract   awesome (todo: dont remove hiphens)
+            filtered_term = re.sub('[^a-zA-Z\d\s-]',' ',lower_term)             # abstract   awesome
             if lower_term != filtered_term:
                 for splitted_term in filtered_term.split(' '):                  # [abstract, awesome]
                     if splitted_term not in filtered_terms:
