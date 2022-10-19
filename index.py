2eb339930bc3e4b96f962b9568367472ed0fa486
Ricardo Rodriguez <ricardorodriguez@ua.pt>
2022-10-19 18:59:07 +0100

    refactor

--- index.py
+++ index.py
@@ -53,15 +53,15 @@ class SPIMIIndexer(Indexer):
 
             tokens = tokenizer.tokenize(pmid, pub_terms)    # tokenize publication
 
-            [self._index.add_term(token, doc_id, index_output_folder=index_output_folder, filename=f'{time.time()}') for token in tokens for doc_id in tokens[token]] # add terms to index
+            [self._index.add_term(token, doc_id, int(counter), index_output_folder=index_output_folder, filename=f'{time.time()}') for token,data in tokens.items() for doc_id, counter in data.items()] # add terms to index
 
-            print(f"Using {psutil.virtual_memory().percent}% of memory", end="\r")
+            print(f"Using {psutil.virtual_memory().percent}% of memory")
             # used_memory = psutil.virtual_memory().used >> 20  # in MB
             if psutil.virtual_memory().percent > self.memory_threshold:
-                self._index.write_to_disk(index_output_folder, f'{time.time()}')
+                self._index.write_to_disk(index_output_folder)
                 self._index.clean_index()
 
-        self._index.write_to_disk(index_output_folder, f'{time.time()}')
+        self._index.write_to_disk(index_output_folder)
         self._index.clean_index()
 
 class BaseIndex:
@@ -82,7 +82,6 @@ class BaseIndex:
             self.write_to_disk(kwargs['index_output_folder'], kwargs['filename'])
             self.clean_index()
 
-        # document: [term1, term2, term3, ...]
         if doc_id not in self.posting_list:
             self.posting_list[doc_id] = [term]
         else:
@@ -99,113 +98,158 @@ class BaseIndex:
         print("Writing index to disk...")
         if not os.path.exists(folder):
             os.makedirs(folder)
+
+        if not os.path.exists(folder.replace("/","") + "/metadata.txt"):
             os.mknod(folder.replace("/","") + "/metadata.txt")    # create metadata file
 
-        sorted_index = sorted(self.posting_list.items(), key=lambda x : x[0])   # index sorted by tokens
+        sorted_index = dict(sorted(self.posting_list.items(), key=lambda x : x[0]))   # index sorted by tokens
 
         metadata = self.load_metadata(folder)
 
-        with open(folder.replace("/","") + "/metadata.txt") as meta_file:
-            
-            for line in meta_file:
-
-                data = line.split()
-                metadata.append([data[0]] + [int(element) for element in data[1:]])    # [ token, block_file_id, num_tokens, num_postings ] 
-
         tokens = list(sorted_index.keys())
 
-        for i in range(len(metadata)-1,-1,-1):                  # reverse for
+        blocks = []
 
-            block_terms = [token for token in tokens if token > metadata[i][0]]
+        if not metadata:
+
+            blocks = self.get_block_chunks(tokens, sorted_index)
+
+        else:
+
+            # if there's a metadata file with content
+
+            for i in range(len(list(metadata))-1,-1,-1):                  # reverse for
+
+                block_terms = [token for token in tokens if token > metadata[i][0]]
+                
+                block_index = { token : sorted_index[token] for token in block_terms }
+
+                # read block and get block index to merge
+                with open(folder.replace("/","") + "/BLOCK_" + str(metadata[i][1]) + ".txt", "r+") as block_file:
+
+                    for line in block_file:
+
+                        line = line.split()
+
+                        term = line[0]
+                        postings_list = { posting.split(":")[0] : posting.split(":")[1] for posting in line[1:] }
+
+                        if term not in block_index:
+
+                            block_index[term] = postings_list
+
+                        else:
+
+                            block_index[term] |= postings_list
+
+                    block_terms = sorted(block_index)      # sorted key list
+
+                    blocks = self.get_block_chunks(block_terms, block_index)
+
+                    if len(blocks) == 1:
+
+                        # no need for additional block, only merge the index
+
+                        block_file.truncate(0)      # erase file content
+
+                        for term in block_terms:
+                            
+                            block_file.write(term + " " + " ".join([ str(k) + ":" + str(v) for k,v in block_index[term].items()] ) + "\n" )     # term 19334:2 29193:1 348243:4 ....
+
+                        blocks = []
+
+                        chunk_keys = block_terms
+                        initial_token = chunk_keys[0]
+                        num_tokens = len(block_terms)
+                        num_postings = sum([len(postings) for postings in block_index.values()])
+
+                        idx = None
+                        for i, item in enumerate(metadata):
+                            if item[1] == metadata[i][1]:
+                                idx = i
+
+                        metadata[idx] = [initial_token, metadata[i][1], num_tokens, num_postings]
+                        metadata.sort(key = lambda x : x[0])    # sort by initial token
+                        self.write_metadata(folder, metadata)
+
+                if blocks:
+
+                    os.remove(folder.replace("/","") + "/BLOCK_" + metadata[i][1] + ".txt")
+
+                    metadata.pop(i)                        
             
-            block_index = { token : sorted_index[token] for token in block_terms }
+        # exists a list of block indexes ready to be written on disk
+        if blocks:
 
-            blocks = []
+            for block in blocks:
 
-            # read block and get block index to merge
-            with open(folder.replace("/","") + "/BLOCK_" + metadata[i][1] + ".txt") as block_file:
+                self.block_counter += 1
 
-                for line in block_file:
+                filename = folder.replace("/","") + "/BLOCK_" + str(self.block_counter) + ".txt"
 
-                    line = line.split()
+                if not os.path.exists(filename):
+                    os.mknod(filename)
 
-                    term = line[0]
-                    postings_list = { posting.split(":")[0] : posting.split(":")[1] for posting in line[1:] }
+                with open(filename, "r+") as chunk_file:
 
-                    if term not in block_index:
+                    for k,v in block.items():
 
-                        block_index[term] = postings_list
+                        chunk_file.write(k + " " + " ".join([ str(el[0]) + ":" + str(el[1]) for el in v]) + "\n" )
 
-                    else:
+                chunk_keys = list(block.keys())
+                initial_token = chunk_keys[0]
+                num_tokens = len(chunk_keys)
+                num_postings = sum([len(postings) for postings in block.values()])
 
-                        block_index[term] += postings_list
+                metadata.append([initial_token, self.block_counter, num_tokens, num_postings])
+                metadata.sort(key = lambda x : x[0])    # sort by initial token
+                self.write_metadata(folder, metadata)
 
-                num_blocks = 1
 
-                if len(list(block_index.keys()) > self.BLOCK_TERMS_LIMIT) or ( self._posting_threshold and sum([len(postings) for postings in block_index.values()] > self._posting_threshold )):
+    def get_blocks_number(self, index):
 
-                    terms_ratio = math.ceil(len(list(block_index.keys()) / self.BLOCK_TERMS_LIMIT))
-                    postings_list_ratio = math.ceil(sum([len(postings) for postings in block_index.values()] ) / self._posting_threshold) if self._posting_threshold else 1
+        index_terms = list(index.keys())
 
-                    num_blocks = max([terms_ratio, postings_list_ratio])
+        num_blocks = 1
 
-                block_index = sorted(self.block_index.items(), key=lambda x : x[0])   
+        if (len(index_terms) > self.BLOCK_TERMS_LIMIT) or ( self._posting_threshold and (sum([len(postings) for postings in index.values()]) > self._posting_threshold )):
 
-                if num_blocks != 1:
+            terms_ratio = math.ceil(len(index_terms) / self.BLOCK_TERMS_LIMIT)
+            postings_list_ratio = math.ceil(sum([len(postings) for postings in index.values()] ) / self._posting_threshold) if self._posting_threshold else 1
 
-                    chunk_length = math.floor(len(list(block_index.keys())) / num_blocks)
-
-                    for i in range(num_blocks):
-
-                        chunk_terms = block_index[i*chunk_length:(i+1)*chunk_length]
-
-                        chunk_index = { term : block_index[term] for term in list(chunk_terms.items())  }
-
-                        blocks.append(chunk_index)
-
-                else:
-
-                    # no need for additional block, only merge the index
-
-                    block_file.truncate(0)      # erase file content
-
-                    for k,v in block_index.items():
-                        
-                        block_file.write(k + " " + " ".join([ str(el[0]) + ":" + str(el[1]) for el in v]+ "\n") )     # term 19334:2 29193:1 348243:4 ....
-
-                    blocks.append(block_index)
-
-                # UPDATE METADATA
+            num_blocks = max([terms_ratio, postings_list_ratio])
             
-            # exists a list of block indexes ready to be written on disk
-            if blocks:
+        return num_blocks
 
-                os.remove(folder.replace("/","") + "/BLOCK_" + metadata[i][1] + ".txt")
+    def get_block_chunks(self, terms, index):
 
-                for block in blocks:
+        blocks = []
 
-                    self.block_counter += 1
+        num_blocks = self.get_blocks_number(index)
 
-                    with open(folder.replace("/","") + "/BLOCK_" + self.block_counter + ".txt") as chunk_file:
+        if num_blocks != 1:
 
-                        for k,v in block:
+            chunk_length = math.floor(len(terms) / num_blocks)
 
-                            chunk_file.write(k + " " + " ".join([ str(el[0]) + ":" + str(el[1]) for el in block]) + "\n" )
+            for i in range(num_blocks):
 
-                    chunk_keys = list(block.keys())
-                    initial_token = chunk_keys[0]
-                    num_tokens = len(chunk_keys)
-                    num_postings = sum([len(postings) for postings in block.values()])
+                chunk_terms = terms[i*chunk_length:(i+1)*chunk_length]
 
-                    metadata.append([initial_token, self.block_counter, num_tokens, num_postings])
-                    metadata = sorted(metadata, lambda x : x[0])    # sort by initial token
-                    self.write_metadata(folder, metadata)
-            
+                chunk_index = { term : index[term] for term in chunk_terms  }
+
+                blocks.append(chunk_index)
+        
+        else:
+
+            blocks.append(index)
+        
+        return blocks
+
     def load_metadata(self, folder):
 
         metadata = []
 
-        with open(folder.replace("/","") + "/metadata.txt") as meta_file:
+        with open(folder.replace("/","") + "/metadata.txt", "r+") as meta_file:
             
             for line in meta_file:
 
@@ -216,13 +260,13 @@ class BaseIndex:
     
     def write_metadata(self, folder, metadata):
 
-        with open(folder.replace("/","") + "/metadata.txt") as meta_file:
+        with open(folder.replace("/","") + "/metadata.txt", "r+") as meta_file:
             
             meta_file.truncate(0)
  
             for line in metadata:
 
-                meta_file.write(" ".join(line) + "\n")
+                meta_file.write(" ".join([str(el) for el in line]) + "\n")
 
     @classmethod
     def load_from_disk(cls, path_to_folder:str):
@@ -244,11 +288,13 @@ class InvertedIndex(BaseIndex):
             self.write_to_disk(kwargs['index_output_folder'], kwargs['filename'])
             self.clean_index()
 
+        term_count = args[0]
+
         # term: [doc_id1, doc_id2, doc_id3, ...]
         if term not in self.posting_list:
-            self.posting_list[term] = [doc_id]
+            self.posting_list[term] = { doc_id : term_count }
         else:
-            self.posting_list[term].append(doc_id)
+            self.posting_list[term] += { doc_id : term_count }
 
     @classmethod
     def load_from_disk(cls, path_to_folder:str):
