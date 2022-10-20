7113d3f379cda4ec2858637062ee655996fe409a
goncalo-leal <goncalolealsilva@ua.pt>
2022-10-20 21:53:23 +0100

    fix bash script

--- assignment1.sh
+++ assignment1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # bash command to solve the assignment 1
-python main.py indexer collections/pubmed_tiny.jsonl pubmedSPIMIindexTiny --tk.minL 2 --tk.stemmer potterNLTK --indexer.token_threshold 30000
-python main.py indexer collections/pubmed_small.jsonl pubmedSPIMIindexSmall --tk.minL 2 --tk.stemmer potterNLTK --indexer.token_threshold 30000
-python main.py indexer collections/pubmed_medium pubmedSPIMIindexMedium --tk.minL 2 --tk.stemmer potterNLTK --indexer.token_threshold 30000
-python main.py indexer collections/pubmed_large pubmedSPIMIindexLarge --tk.minL 2 --tk.stemmer potterNLTK --indexer.token_threshold 30000
+python main.py indexer collections/pubmed_2022_tiny.jsonl.gz pubmedSPIMIindexTiny --tk.minL 2 --tk.stemmer potterNLTK --indexer.token_threshold 30000
+python main.py indexer collections/pubmed_2022_small.jsonl.gz pubmedSPIMIindexSmall --tk.minL 2 --tk.stemmer potterNLTK --indexer.token_threshold 30000
+python main.py indexer collections/pubmed_2022_medium.jsonl.gz pubmedSPIMIindexMedium --tk.minL 2 --tk.stemmer potterNLTK --indexer.token_threshold 30000
+python main.py indexer collections/pubmed_2022_large.jsonl.gz pubmedSPIMIindexLarge --tk.minL 2 --tk.stemmer potterNLTK --indexer.token_threshold 30000
